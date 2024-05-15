1dbeeb297a7ff1862e9a32bb4b487f7f311ec827
linhuni <119025871+linhuni@users.noreply.github.com>
2024-05-15 11:44:16 +0700

    m

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.javafx to javafx.fxml;
+    exports com.example.javafx;
+}
